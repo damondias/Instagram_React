0fa3f9a1cc45409460746b8ec15c84133be2c482
Damon <damondias@gmail.com>
2023-05-08 14:08:46 -0400

    feat: dynamic data of the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import Suggestions from "./Suggestions";
 
 function Sidebar(){
+    let [name, setName] = useState("catanacomics"); 
+    let [image, setImage] = useState("assets/img/catanacomics.svg");
+    
+    function changeName() {
+        let newName = prompt('Qual é o seu nome?');
+        if (newName) setName(newName);
+    }
+
+    function changeImage(){
+        let newImage = prompt('Digite a nova imagem');
+        if (newImage) setImage(newImage);
+    }
+
     return(
         <div class="sidebar">
             <div class="usuario">
-                <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+                <img onClick={changeImage} src={image} alt="imagem de perfil"/>
                 <div class="texto">
                 <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{name}</strong>
+                    <ion-icon onClick={changeName} name="pencil"></ion-icon>
                 </span>
                 </div>
             </div>
